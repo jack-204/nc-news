9a81101ec3ec6c3b999f55db374699a8be0a5c35
Jack Kent <jack.kent498@gmail.com>
2024-01-15 14:47:19 +0000

    adds an /api endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const app = require('../app')
 const db = require('../db/connection')
 const seed = require('../db/seeds/seed')
 const data = require('../db/data/test-data/index')
+const fs = require('fs/promises')
 
 beforeEach(() => {
     return seed(data)
@@ -23,6 +24,23 @@ describe('app.js', () => {
                     expect(topic.hasOwnProperty('slug')).toBe(true)
                     expect(topic.hasOwnProperty('description')).toBe(true)
                 })
+            })
+        })
+    })
+
+    describe('GET /api', () => {
+        test('200: responds with an object of endpoints', () => {
+            return request(app).get('/api')
+            .expect(200)
+            .then(({body}) => {
+                
+                const file =  fs.readFile('./endpoints.json', 'utf-8')
+                return Promise.all([file, body])
+            })
+            .then((contents) => {
+                const file = JSON.parse(contents[0])
+                const body = contents[1].endpoints
+                expect(body).toEqual(file)
             })
         })
     })
--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 
 const {getTopics} = require('./controllers/topics.controllers')
+const {getEndpoints} = require('./controllers/api.controllers')
 
 app.get('/api/topics', getTopics)
+
+app.get('/api', getEndpoints)
 
 module.exports = app
