5a1402d4da58f5d938a8bf5dc69bff0c632c2d81
Jack Kent <jack.kent498@gmail.com>
2024-01-19 14:17:09 +0000

    18 adds update-comment-votes

--- routes/comments-router.js
+++ routes/comments-router.js
@@ -1,7 +1,9 @@
 const commentsRouter = require('express').Router()
 
-const {deleteCommentById} = require('../controllers/comments.controllers')
+const {deleteCommentById, patchCommentVotes} = require('../controllers/comments.controllers')
 
 commentsRouter.delete('/:comment_id', deleteCommentById)
+
+commentsRouter.patch('/:comment_id', patchCommentVotes)
 
 module.exports = commentsRouter
